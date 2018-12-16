c7ccd58469ee9b40bc1b5364012173bcfa16864b
kozakvasyl <koretsua@gmail.com>
2018-12-16 16:28:14 +0200

    added new prosucts

--- supermarket.js
+++ supermarket.js
@@ -17,9 +17,17 @@ Supermarket.prototype.bill = function(price, weight) {
 
 var products = [
     new Supermarket('Apple', 'fruits', 17, 5, 'http://icons.iconarchive.com/icons/bingxueling/fruit-vegetables/256/apple-red-icon.png'),
-    new Supermarket('Lemon', 'vegetables', 9, 7, 'http://www.pngmart.com/files/1/Lemon-PNG-Clipart.png'),
-    new Supermarket('Cake', 'sweets', 2, 25, 'https://assimdoce.com.br/wp-content/uploads/2016/03/bolo.png')
+    new Supermarket('Potato', 'vegetables', 9, 7, 'https://www.freepngimg.com/thumb/potato/5-2-potato-png-clipart-thumb.png'),
+    new Supermarket('Cake', 'sweets', 2, 25, 'https://assimdoce.com.br/wp-content/uploads/2016/03/bolo.png'),
+    new Supermarket('Сandy', 'sweets', 33, 19, 'https://s3.amazonaws.com/peoplepng/wp-content/uploads/2018/10/19171518/Candy-Vector-Free-Transparent.png'),
+    new Supermarket('Сhery', 'sweets', 30, 5, 'https://gallery.yopriceville.com/var/albums/Free-Clipart-Pictures/Fruit-PNG/Cherries_PNG_Clipart_Picture-1848659855.png?m=1434276910'),
+    new Supermarket('Roll', 'sweets', 21, 6, 'https://www.cobsbread.com/drive/uploads/2018/01/cobs-product-white-bun-650x458.png'),
+    new Supermarket('Сandy', 'sweets', 15, 8, ''),
+    new Supermarket('Сandy', 'sweets', 15, 8, ''),
+    new Supermarket('Сandy', 'sweets', 15, 8, ''),
+    new Supermarket('Сandy', 'sweets', 15, 8, ''),
 ];
+
 
 function render() {
     var market = document.querySelector('.market');
